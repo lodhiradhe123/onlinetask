6c6b748ef05881399bca4748c74d04f62f65c839
Radheshyam lodhi <96337637+lodhiradhe123@users.noreply.github.com>
2024-06-23 14:47:07 +0530

    Update connetc.js

--- models/connetc.js
+++ models/connetc.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 require('dotenv').config();
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect("mongodb+srv://radheshyamlodhi48:WDVwoo3fb5U9zkln@cluster0.66ujv2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("connected");
 }).catch((err)=>{
     console.log(err);
